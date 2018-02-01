cca5b9cf7b5b0be993a38fac592994b522f99521
JP <jc7553a@student.american.edu>
2018-02-01 14:24:22 -0500

    Cleaned up Cluster

--- cluster.py
+++ cluster.py
@@ -30,27 +30,6 @@ def test(auto, data):
         res.append(auto.calc_total_cost([data[i+half][0:len(data[i])-1]]))
     return res
 
-def split(res, splits):
-    resMatrix = []
-    for i in range(len(res)):
-        holder = []
-        holder.append(res[i])
-        holder.append(i)
-        resMatrix.append(holder)
-    resMatrix = np.array(resMatrix)
-    resMatrix[resMatrix[:,1].argsort()]
-    returnTensor = []
-    for i in range(len(splits)):
-        holder = []
-        j = 0
-        while j <(len(res)):
-            if res[j][0] < splits[i]:
-                holder.append(res[j])
-                del res[j]
-                j -=1
-            j+=1
-        returnTensor.append(holder)
-    return returnTensor
 def onlineTrain(data, network):
     start = int(len(data)/2)
     losses = []
@@ -62,21 +41,6 @@ def onlineTrain(data, network):
     splits = askQuestions(losses)
     return splits
                 
-
-def askQuestions(errors):
-    print("Do you want a visual graph? Type 0 for yes")
-    graph = int(input())
-    if graph == 0:
-        plt.plot(errors)
-        plt.show()
-    print("How many splits do you see?")
-    splits = input()
-    splits = int(splits)
-    splitsVals = []
-    for i in range(splits):
-        print("Where does Split " + str(i+1) + " begin?")
-        splitsVals.append(float(input()))
-    return splitsVals
 
 def childrenTrain(network, children, data):
     print("Got To Gating")
